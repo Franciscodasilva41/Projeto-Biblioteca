3b8ae711ef9f422a3afba8ffbd573719bc705b2f
Francisco <francisosoares2084@outlook.com>
2026-04-12 11:38:20 -0300

    Atualização da classe Emprestimo foi criado metodo devoluçao e listar devolução

--- src/biblioteca/dao/EmprestimoDAO.java
+++ src/biblioteca/dao/EmprestimoDAO.java
@@ -6,6 +6,7 @@ package biblioteca.dao;
 import java.sql.*;
 import biblioteca.database.Conexao;
 
+
 /**
  *
  * @author Usuario
@@ -47,4 +48,50 @@ public class EmprestimoDAO {
             e.printStackTrace();
         }
     }
+     public void devolver(int idEmprestimo, int idLivro){
+        
+      String sqlEmprestimo = "UPDATE emprestimo SET data_devolucao = NOW()WHERE id = ?";
+       String sqlLivro = "UPDATE livro SET disponivel = true WHERE id = ?";
+       
+       try(Connection conn = Conexao.conectar()){
+        conn.setAutoCommit(false);// transação
+        PreparedStatement stmt1 = conn.prepareStatement(sqlEmprestimo);
+        stmt1.setInt(1,idEmprestimo);
+        stmt1.executeUpdate();
+        
+        PreparedStatement stmt2 = conn.prepareStatement(sqlLivro);
+        stmt2.setInt(1, idLivro);
+        stmt2.executeUpdate();
+          conn.commit();
+           System.out.println("Devolução realizada com sucesso!");
+        
+       }catch (SQLException e){
+       e.printStackTrace();
+       }
+     }
+     public void listarEmprestimo(){
+       String sql = "SELECT * FROM emprestimo";
+       
+       try(Connection conn = Conexao.conectar();
+           PreparedStatement stmt = conn.prepareStatement(sql);
+           ResultSet rs = stmt.executeQuery()){
+       
+         while(rs.next()){
+         int id = rs.getInt("id");
+         int idUsuario = rs.getInt("id_usuario");
+         int idLivro = rs.getInt("id_livro");
+         Timestamp dataEmprestimo = rs.getTimestamp("data_emprestimo");
+         Timestamp dataDevolucao = rs.getTimestamp("data_devolucao");
+             System.out.println("ID: "+ id);
+             System.out.println("Usuario: "+ idUsuario);
+             System.out.println("Livro: "+ idLivro);
+             System.out.println("Data Emprestimo: "+ dataEmprestimo);
+             System.out.println("Data Devolução: "+ dataDevolucao);
+             System.out.println("------------------------------------------");
+             
+         }
+       }catch(SQLException e){
+       e.printStackTrace();
+       }
+     }
 }
